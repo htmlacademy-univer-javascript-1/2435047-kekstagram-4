505f7a4a1c068d3773dad91728dfed696045456b
Keks <keks@htmlacademy.ru>
2023-11-14 14:42:08 +0300

    :heavy_check_mark: Сборка #6

--- 6/js/pictures.js
+++ 6/js/pictures.js
@@ -3,13 +3,13 @@ const pictures = document.querySelector('.pictures');
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
 const renderPhoto = (photo) => {
-  const {url, description, likes, comments} = photo;
+  const {url,  likes, comments, description} = photo;
   const newPhoto = pictureTemplate.cloneNode(true);
 
   newPhoto.querySelector('.picture__img').src = url;
   newPhoto.querySelector('.picture__img').alt = description;
-  newPhoto.querySelector('.picture__comments') = comments;
-  newPhoto.querySelector('.picture__likes') = likes;
+  newPhoto.querySelector('.picture__likes').textContent = likes;
+  newPhoto.querySelector('.picture__comments').textContent = comments;
 
   return newPhoto;
 };
