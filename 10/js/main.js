eeb341edeb31d630b4b0dfe1a07093a80ca6ec06
Keks <keks@htmlacademy.ru>
2023-11-30 12:40:53 +0300

    :heavy_check_mark: Сборка #10

--- 10/js/main.js
+++ 10/js/main.js
@@ -2,5 +2,6 @@ import { photos } from './data.js';
 import { renderPhotos } from './pictures.js';
 import  './form.js';
 import './hashtags-prestine.js';
+import './effects.js';
 
 renderPhotos(photos);
