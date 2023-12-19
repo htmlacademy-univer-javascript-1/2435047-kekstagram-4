68e0e1f7cc1a8126437db977ece55cb72d20fe56
Andrew 61X Tiunov <astermiizek@gmail.com>
2023-12-19 20:22:59 +0500

    11.14 Надо подкачаться

--- js/main.js
+++ js/main.js
@@ -1,7 +1,30 @@
-import { photos } from './data.js';
 import { renderPhotos } from './pictures.js';
 import  './form.js';
 import './effects.js';
 import './hashtags-pristine.js';
+import { loadData } from './api.js';
 
-renderPhotos(photos);
+let photos = [];
+
+const onSuccess = (data) => {
+  photos = data.slice();
+  renderPhotos(photos);
+  document.querySelector('.img-filters').classList.remove('img-filters--inactive');
+};
+
+const onError = () => {
+  const messageAlert = document.createElement('div');
+  messageAlert.style.position = 'absolute';
+  messageAlert.style.left = 0;
+  messageAlert.style.top = 0;
+  messageAlert.style.right = 0;
+  messageAlert.style.fontSize = '30px';
+  messageAlert.style.backgroundColor = 'red';
+  messageAlert.style.textAlign = 'center';
+  messageAlert.textContent = 'Ошибка загрузки фотографий';
+  document.body.append(messageAlert);
+};
+
+loadData(onSuccess, onError);
+
+export {photos};
